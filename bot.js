f471c3c89aa2ad6d3bc4bc84ee7b604cdcde8b98
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-11-23 16:51:33 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,145 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('message', async message => {
+    let args = message.content.split(" ");
+    let command = args[0];
+
+    if(command === prefix + 'ban') {
+      if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply('انت لا تملك الصلاحيات اللازمة').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+
+      if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply('انا لا املك الصلاحيات اللازمة. يحب توفر صلاحيات `Ban Members , Embed Links`').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+      let mention = message.mentions.members.first();
+      if(!mention) return message.reply('**منشن عضو لطرده**').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+      if(mention.highestRole.position >= message.guild.member(message.author).highestRole.positon) return message.reply('**لا يمكنك طرد شخص رتبته اعلى منك**').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+      if(mention.highestRole.positon >= message.guild.member(client.user).highestRole.positon) return message.reply('**لا يمكنني طرد شخص رتبته اعلى مني**').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+      if(mention.id === message.author.id) return message.reply('**لا يمكنك طرد  نفسك**').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+
+       let duration = args[2];
+       if(!duration) return message.reply('**حدد وقت زمني لفك البان عن الشخص**').then(msg => {
+         msg.delete(3500);
+         message.delete(3500);
+       });
+       if(isNaN(duration)) return message.reply('**حدد وقت زمني صحيح**').then(msg => {
+         msg.delete(3500);
+         message.delete(3500);
+       });
+
+       let reason = message.content.split(" ").slice(3).join(" ");
+       if(!reason) reason = 'غير محدد';
+
+       let thisEmbed = new Discord.RichEmbed()
+       .setAuthor(mention.user.username , mention.user.avatarURL)
+       .setTitle('لقد تبندت من سيرفر')
+       .setThumbnail(mention.avatarURL)
+       .addField('# - السيرفر:',message.guild.name,true)
+       .addField('# - تم طردك بواسطة',message.author,true)
+       .addField('# - السبب',reason)
+       .setFooter(message.author.tag,message.author.avatarURL);
+       mention.send(thisEmbed).then(() => {
+       mention.ban({
+         reason: reason,
+       });
+       message.channel.send(`**:white_check_mark: ${mention.user.username} banned from the server ! :airplane: **  `)
+       setTimeout(() => {
+         if(duration === 0) return;
+         message.guild.unban(mention);
+       },duration * 60000);
+     });
+   }
+});
+
+client.on('message', message => {
+var prefix = "*";
+  if (message.author.kick) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command == "kick") {
+               if(!message.channel.guild) return;
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+ 
+  if (message.mentions.users.size < 1) return message.reply("منشن شخص");
+  if(!reason) return message.reply ("اكتب سبب الطرد");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("لايمكنني طرد شخص اعلى من رتبتي");
+ 
+  message.guild.member(user).kick(7, user);
+ 
+  const banembed = new Discord.RichEmbed()
+  .setAuthor('Kicked !', user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("User:",  `[ + ${user.tag} + ]`)
+  .addField("By:", `[  + ${message.author.tag} +  ]`)
+  .addField("Reason:", `[ + ${reason} +  ]`)
+  client.channels.get("492583022982463500").send({embed : banembed})
+}
+});
+
+        client.on('message', async message => {
+            let muteReason = message.content.split(" ").slice(3).join(" ");
+            let mutePerson = message.mentions.users.first();
+            let messageArray = message.content.split(" ");
+            let muteRole = message.guild.roles.find("name", "Muted");
+            let time = messageArray[2];
+            if(message.content.startsWith(prefix + "mute")) {
+                if(!message.member.hasPermission('MUTE_MEMBERS')) return message.channel.send('**Sorry But You Dont Have Permission** `MUTE_MEMBERS`' );
+                if(!mutePerson) return message.channel.send('**Mention Someone**')
+                if(mutePerson === message.author) return message.channel.send('**You Cant Mute Yourself**');
+                if(mutePerson === client.user) return message.channel.send('**You Cant Mute The Bot**');
+                if(message.guild.member(mutePerson).roles.has(muteRole.id)) return message.channel.send('**This Person Already Tempmuted !**');
+                if(!muteRole) return message.guild.createRole({ name: "Muted", permissions: [] });
+                if(!time) return message.channel.send("**Type The Duration**");
+                if(!time.match(/[1-60][s,m,h,d,w]/g)) return message.channel.send('**The Bot Not Support This Time**');
+                if(!muteReason) return message.channel.send('Please Type The Reason')
+                message.guild.member(mutePerson).addRole(muteRole);
+                message.channel.send(`**:white_check_mark: ${mutePerson} has been muted ! :zipper_mouth: **`)
+                message.delete()
+                let muteEmbed = new Discord.RichEmbed()
+                .setTitle(`New Temp Muted User`)
+                .setThumbnail(message.guild.iconURL)
+                .addField('Muted By:',message.author,true)
+                .addField('Muted User:', `${mutePerson}`)
+                .addField('Reason:',muteReason,true)
+                .addField('Duration:',`${mmss(mmss(time), {long: true})}`)
+                .setFooter(message.author.username,message.author.avatarURL);
+                let logchannel = message.guild.channels.find(`name`, "log");
+                if(!logchannel) return message.channel.send("Can't find log channel.");
+                logchannel.sendEmbed(muteEmbed)
+                mutePerson.send(`**You Are has been temp muted in ${message.guild.name} reason: ${muteReason}**`)
+                .then(() => { setTimeout(() => {
+                   message.guild.member(mutePerson).removeRole(muteRole);
+               }, mmss(time));
+            });
+            }
+        });
+
+client.login(process.env.BOT_TOKEN);
